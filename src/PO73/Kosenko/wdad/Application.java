f3aff0fe8c12ed0695b6e4844e9858477003217a
DaryaKosenko <KosenkoDarya64@gmail.com>
2019-09-26 16:03:36 +0400

    Added entry point

--- src/PO73/Kosenko/wdad/Application.java
+++ src/PO73/Kosenko/wdad/Application.java
@@ -2,5 +2,6 @@ package PO73.Kosenko.wdad;
 
 public class Application {
     public static void main(String[] args){
-    System.out.println("I’m Darya Kosenko, and I’m not a monkey");}
+    System.out.println("I’m Darya Kosenko, and I’m not a monkey");
+    }
 }
